d54fd4cc04cc0fb5f5d19eefa27a90fc205ffd04
vivekanand singh <vs9727601@gmail.com>
2026-09-12 18:51:57 +0530

    Time: 0 ms (100%), Space: 10.2 MB (77.26%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int ans = 0;
+        int prev = 0;
+
+        for (int i = s.size() - 1; i >= 0; --i) {
+            int curr;
+
+            switch (s[i]) {
+                case 'I': curr = 1;    break;
+                case 'V': curr = 5;    break;
+                case 'X': curr = 10;   break;
+                case 'L': curr = 50;   break;
+                case 'C': curr = 100;  break;
+                case 'D': curr = 500;  break;
+                case 'M': curr = 1000; break;
+            }
+
+            if (curr < prev)
+                ans -= curr;
+            else
+                ans += curr;
+
+            prev = curr;
+        }
+
+        return ans;
+    }
+};
